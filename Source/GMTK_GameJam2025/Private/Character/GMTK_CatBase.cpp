cdc987bef12179202edf4c7ed957dfc13edfe34e
Chukwuyenum Opone <oponechukwuyenum@gmail.com>
2025-08-02 17:35:53 +0100

    Merge branch 'main' of https://github.com/scytestudio/GMTK_GameJam2025

--- Source/GMTK_GameJam2025/Private/Character/GMTK_CatBase.cpp
+++ Source/GMTK_GameJam2025/Private/Character/GMTK_CatBase.cpp
@@ -4,6 +4,7 @@
 #include "Character/GMTK_CatBase.h"
 
 #include "PaperFlipbookComponent.h"
+#include "Components/CapsuleComponent.h"
 #include "Components/SceneCaptureComponent2D.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
@@ -33,11 +34,45 @@ void AGMTK_CatBase::UpdateDirection(float MoveDirection)
 	}
 }
 
-void AGMTK_CatBase::OnDashOverrideAnimEnd(bool Completed)
+
+
+void AGMTK_CatBase::ShrinkCat()
 {
-	bCanDash = true;
-	
-	UE_LOG(LogTemp, Warning, TEXT("Dash Completed"));
+	if (bCanShrink && bCanMove && !GetCharacterMovement()->IsFalling())
+	{
+		if (!ShrinkAnimSequence) return;
+		UE_LOG(LogTemp, Warning, TEXT("Initiate Shrink"));
+		bCanShrink = false;
+		GetCapsuleComponent()->SetCapsuleSize(15.f, 88.f);
+		GetAnimInstance()->PlayAnimationOverride(ShrinkAnimSequence, FName("DefaultSlot"), 1.0f, 0.0f, OnShrinkOverrideEndDelegate);
+	}
+}
+
+void AGMTK_CatBase::OnShrinkOverrideAnimEnd(bool Completed)
+{
+	GetCapsuleComponent()->SetCapsuleSize(34.f, 88.f);
+	bCanShrink = true;
+}
+
+void AGMTK_CatBase::VerticalJumpCat()
+{
+	if (bCanVerticalJump && bCanMove && !GetCharacterMovement()->IsFalling())
+	{
+		if (!VerticalJumpAnimSequence) return;
+		UE_LOG(LogTemp, Warning, TEXT("Initiate Vertical Jump"));
+		bCanVerticalJump = false;
+		float LaunchVelocityX = bFacingRight ? 1000.0f : -1000.0f;
+		float LaunchVelocityZ = 1000.0f;
+		FVector LaunchVelocity = FVector(LaunchVelocityX, 0.0f, LaunchVelocityZ);
+		LaunchCharacter(LaunchVelocity, false, false);
+		GetAnimInstance()->PlayAnimationOverride(VerticalJumpAnimSequence, FName("DefaultSlot"), 1.0f, 0.0f, OnVerticalJumpOverrideEndDelegate);
+	}
+}
+
+
+void AGMTK_CatBase::OnVerticalJumpOverrideAnimEnd(bool Completed)
+{
+	bCanVerticalJump = true;
 }
 
 void AGMTK_CatBase::MoveCat(float Direction)
@@ -55,13 +90,19 @@ void AGMTK_CatBase::DashCat()
 	{
 		UE_LOG(LogTemp, Warning, TEXT("Initiate Dash"));
 		bCanDash = false;
+		GetCharacterMovement()->MaxWalkSpeed = 1000.0f;
 		float LaunchVelocityX = bFacingRight ? 1000.0f : -1000.0f;
-		float LaunchVelocityZ = 1000.0f;
-		FVector LaunchVelocity = FVector(LaunchVelocityX, 0.0f, LaunchVelocityZ);
+		FVector LaunchVelocity = FVector(LaunchVelocityX, 0.0f, 0.0f);
 		LaunchCharacter(LaunchVelocity, false, false);
 		GetAnimInstance()->PlayAnimationOverride(DashAnimSequence, FName("DefaultSlot"), 1.0f, 0.0f, OnDashOverrideEndDelegate);
-		
 	}
+}
+
+void AGMTK_CatBase::OnDashOverrideAnimEnd(bool Completed)
+{
+	bCanDash = true;
+	GetCharacterMovement()->MaxWalkSpeed = 600.0f;
+	UE_LOG(LogTemp, Warning, TEXT("Dash Completed"));
 }
 
 void AGMTK_CatBase::JumpCat()
@@ -77,4 +118,6 @@ void AGMTK_CatBase::BeginPlay()
 	Super::BeginPlay();
 
 	OnDashOverrideEndDelegate.BindUObject(this, &AGMTK_CatBase::OnDashOverrideAnimEnd);
+	OnShrinkOverrideEndDelegate.BindUObject(this, &AGMTK_CatBase::OnShrinkOverrideAnimEnd);
+	OnVerticalJumpOverrideEndDelegate.BindUObject(this, &AGMTK_CatBase::OnVerticalJumpOverrideAnimEnd);
 }
